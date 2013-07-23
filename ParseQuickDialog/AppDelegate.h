a292e7b22f5c4570d91d4755ff078d5b5eea0575
pyro2927 <joseph@pintozzi.com>
2013-07-22 21:17:35 -0500

    Initial Commit

---
+++ ParseQuickDialog/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ParseQuickDialog
+//
+//  Created by Joseph Pintozzi on 7/22/13.
+//  Copyright (c) 2013 TinyDragonApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
